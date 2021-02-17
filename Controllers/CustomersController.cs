727311a3d2809e1db4ea57313029df6e2c107620
Mitesh <cool.miteshjha@gmail.com>
2021-02-17 22:00:26 +0530

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.ViewModels;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Customers()
+        {
+            var customers = new List<Customers>
+            {
+                new Customers { Name = "Customer1"},
+                new Customers { Name = "Customer2"},
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Controllers/MovieController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Movies()
+        {
+            //var movie = new Movie()
+            //{
+            //    Name = "Shrek!"
+            //};
+            var movie = new List<Movie>
+            {
+                new Movie{ Name = "Shrek!"},
+                new Movie { Name = "Ghost"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+
+                Movie = movie
+            };
+            return View(viewModel);
+
+            //var customers = new List<Customers>
+            //{
+            //    new Customers { Name = "Customer1"},
+            //    new Customers { Name = "Customer2"},
+            //};
+
+            //var viewModel = new RandomMovieViewModel
+            //{
+            //    Movie = movie,
+            //    Customers=customers
+            //};
+            //return View(viewModel);
+        }
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)            
+        //{                                                             //using attribute routing with applying constraints
+        //    return Content(year + "/" + month);
+        //}
+    }
+}
