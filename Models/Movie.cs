df9cd933d6e45a258a7e817ca6c6bf20b1a68bdd
Mitesh <cool.miteshjha@gmail.com>
2021-02-27 11:39:34 +0530

    New Commits to Add Movie

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -17,7 +17,7 @@ namespace Vidly.Models
 
         [Display(Name ="Genre")]
         [Required]
-        public byte GenreId { get; set; }
+        public int GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
 
@@ -26,6 +26,6 @@ namespace Vidly.Models
 
         [Display(Name = "Number in Stock")]
         [Range(1,20)]
-        public byte NumberInStock { get; set; }
+        public int NumberInStock { get; set; }
     }
 }
--- ViewModels/MovieFormViewModel.cs
+++ ViewModels/MovieFormViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Vidly.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.ViewModels
 {
