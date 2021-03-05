3a1afad2625b56364bf277b3cfee437dc1cb5a57
Mitesh <cool.miteshjha@gmail.com>
2021-03-05 23:16:33 +0530

    Delete All Migration and Database than Adding Identity to Existing Project than recreate Migration and update database

---
+++ Migrations/202103051633196_PopulateMemberShipTypes.cs
@@ -0,0 +1,21 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMemberShipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO MembershipTypes(Id,Name, SignUpFee, DuretionInMonths, DiscountRates)VALUES(1,'Pay as You Go',0,0,0)");
+            Sql("INSERT INTO MembershipTypes(Id,Name, SignUpFee, DuretionInMonths, DiscountRates)VALUES(2,'Monthly',30,1,10)");
+            Sql("INSERT INTO MembershipTypes(Id,Name, SignUpFee, DuretionInMonths, DiscountRates)VALUES(3,'Quarterly',90,3,15)");
+            Sql("INSERT INTO MembershipTypes(Id,Name, SignUpFee, DuretionInMonths, DiscountRates)VALUES(4,'Annual',300,12,20)");
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -27,5 +27,6 @@ namespace Vidly.Models
         [Display(Name = "Number in Stock")]
         [Range(1,20)]
         public int NumberInStock { get; set; }
+        public int NumberAvailable { get; set; }
     }
 }
