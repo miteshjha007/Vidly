b752da38b267a7974bdd09c6fc243491523d3697
Mitesh <cool.miteshjha@gmail.com>
2021-03-18 23:27:51 +0530

    CreateAPi for rental Movies

---
+++ Migrations/202103181748251_ModifyNumberInStock.cs
@@ -0,0 +1,17 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ModifyNumberInStock : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE Movies SET NumberAvailable = NumberInStock");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
